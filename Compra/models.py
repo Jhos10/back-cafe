1edc62f8c4c06843dc2e9a5f97fc91947486cc11
Jhos10 <jhostinposada7@gmail.com>
2025-06-04 23:23:01 -0500

    Se hizo la tabla de compra

--- Compra/models.py
+++ Compra/models.py
@@ -4,8 +4,8 @@ from categoriaProducto.models import Producto
 from datetime import date
 # Create your models here.
 class Compra(models.Model):
-    producto=models.ForeignKey(Producto,related_name='compra_producto',on_delete=models.SET_NULL)
-    usuario=models.ForeignKey(User,related_name='compra_usuario',on_delete=models.SET_NULL)
+    producto=models.ForeignKey(Producto,related_name='compra_producto',on_delete=models.SET_NULL,blank=True,null=True)
+    usuario=models.ForeignKey(User,related_name='compra_usuario',on_delete=models.SET_NULL, blank=True, null=True)
     cantidad=models.IntegerField()
     fecha=models.DateField(default=date.today())
 
